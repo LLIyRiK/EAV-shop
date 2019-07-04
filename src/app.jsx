fcb152d16163d700c0bf12d19fa33e85c00f6e1e
Senkevich Alexsandr <a.siankevich@altoros.com>
2019-07-04 13:01:51 +0300

    delete fancy button2

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,6 @@
 import React, {Component, Fragment} from 'react';
 import Button from "./button/button";
 import styles from './styles/main.scss';
-import FancyButton from "./fancy-button/fancy-button";
 
 const content = 'Hello world!';
 
@@ -9,7 +8,6 @@ const App = () => (
   <Fragment>
     <div className={styles["resizeable-both"]}>{content}</div>
     <Button label="Regular Button"/>
-    <FancyButton label="Fancy Button"/>
   </Fragment>
 );
 
